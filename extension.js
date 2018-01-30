25fd6f392cc7916c8e19ff95c9a23bf6e2a6edd7
mcallegari10 <martin.callegari@wolox.com.ar>
2018-01-30 20:20:13 -0300

    add the reading of the babelrc

--- extension.js
+++ extension.js
@@ -6,10 +6,6 @@ const vscode = require('vscode')
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "babel-alias-goto" is now active!')
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
@@ -17,7 +13,11 @@ function activate(context) {
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!')
+    const babelConfigPath = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.babelrc`
+
+    vscode.workspace.openTextDocument(babelConfigPath).then((document) => {
+      console.log(document.getText())
+    }, (error) => console.error(error))
   });
 
   context.subscriptions.push(disposable)
