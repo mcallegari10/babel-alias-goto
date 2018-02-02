f3269fb39f0bbbd97ea23d06b729afe6a9694436
mcallegari10 <martin.callegari@wolox.com.ar>
2018-02-01 21:12:13 -0300

    get of plugins

--- extension.js
+++ extension.js
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 
+const getAliases = babelPlugins =>
+  babelPlugins.map(plugin => plugin[0] === 'module-resolver' ? plugin[1] : undefined)
+              .filter(result => result !== undefined)[0]
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -10,15 +14,15 @@ function activate(context) {
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-    // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
     const babelConfigPath = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.babelrc`
 
-    vscode.workspace.openTextDocument(babelConfigPath).then((document) => {
-      console.log(document.getText())
-    }, (error) => console.error(error))
-  });
+    vscode.workspace.openTextDocument(babelConfigPath).then(document => {
+      const aliasPlugin = getAliases(JSON.parse(document.getText()).plugins)
+    },
+      error => vscode.window.showErrorMessage(error)
+    )
+  })
 
   context.subscriptions.push(disposable)
 }
